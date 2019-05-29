661976364911c21aa2345fb863aed5875f440fc5
zhaojunchen <zjc986812982@163.com>
2019-05-29 10:07:07 +0800

    更新

--- AnnoyWechat/app/src/main/java/com/whu/annoywechat/MainActivity.java
+++ AnnoyWechat/app/src/main/java/com/whu/annoywechat/MainActivity.java
@@ -1,7 +1,11 @@
 package com.whu.annoywechat;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,11 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
 
-        Button button = (Button)
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse("http://www.baidu.com"));
-        startActivity(intent);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.baidu.com"));
+                startActivity(intent);
+            }
+        });
 
 
     }
